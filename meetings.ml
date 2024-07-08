996df7643934bf445dabd8f6226ca06ca89f06be
Vinaya Mandke <vinaya@fairmatic.com>
2024-07-08 21:22:00 +0530

    Minor Updates

--- meetings.ml
+++ meetings.ml
@@ -1,3 +1,30 @@
+(* "package main
+
+import ""fmt""
+
+// countMeetings calculates the total number of meetings that occur between any pairs of given points
+// during their journey from their respective starting to destination points. Given two lists of integers 'a' 
+// and 'b' where 'a[i]' and 'b[i]' are the starting and ending points respectively on a number line for 'n' 
+// distinct points, all points move at a constant speed of 1 unit per second from 'a[i]' to 'b[i]'. A meeting 
+// occurs when any two points 'a[i]' and 'a[j]' (when i != j) intersect at the same point during their journey.
+// Each meeting between any pair of points is counted exactly once.
+//
+// Args:
+//     a ([]int): A list of starting positions of points on a number line.
+//     b ([]int): A list of ending positions of points on a number line.
+//
+// Returns:
+//     int: The total number of meetings that occur between any pairs of the given points
+//          during their journey from their respective starting to destination points.
+//
+// Examples:
+//     fmt.Println(countMeetings([]int{2, 1}, []int{3, 4})) // 1
+//     fmt.Println(countMeetings([]int{-10, -5, -12, -13}, []int{10, 5, 12, 13})) // 6
+//     fmt.Println(countMeetings([]int{1, 3, 5, 7}, []int{2, 4, 6, 8})) // 0
+func countMeetings(a []int, b []int) {" *)
+
+
+
 (* Define a type to represent the state of each point *)
 type point_state = {
   mutable position : int;
