1ec8429b42c55a6f92799f79828d1b5271a87f78
Vinaya Mandke <vinaya@fairmatic.com>
2024-07-10 00:23:11 +0530

    Fixes

--- treasure_hunt.ml
+++ treasure_hunt.ml
@@ -1,4 +1,6 @@
-(* "from typing import List
+(*
+123
+"from typing import List
 
 def shortest_treasure_hunt(grid: List[List[str]]) -> int:
     """"""
@@ -25,73 +27,71 @@ def shortest_treasure_hunt(grid: List[List[str]]) -> int:
 
 
 
+let can_move grid i j =
+  i >= 0 && i < Array.length grid &&
+  j >= 0 && j < Array.length grid.(0) &&
+  grid.(i).(j) <> 'X'
 
-let can_move_left grid i j =
-  j - 1 >= 0 && grid.(i).(j - 1) <> 'X'
-
-let can_move_right grid i j =
-  j + 1 < Array.length grid.(0) && grid.(i).(j + 1) <> 'X'
-
-let can_move_up grid i j =
-  i - 1 >= 0 && grid.(i - 1).(j) <> 'X'
-
-let can_move_down grid i j =
-  i + 1 < Array.length grid && grid.(i + 1).(j) <> 'X'
-
-let possible_paths = ref []
-
-let rec traverse grid sources pending_treasures i j path_count visited =
-  let pending_treasures =
-    if grid.(i).(j) = 'T' then
-      List.filter (fun (x, y) -> (x, y) <> (i, j)) pending_treasures
-    else
-      pending_treasures
-  in
-
-  if grid.(i).(j) = 'S' && List.length pending_treasures = 0 then
-    possible_paths := path_count :: !possible_paths;
-
-  if Hashtbl.mem visited (i, j) && Hashtbl.find visited (i, j) = List.length pending_treasures then
-    (* give up; futile loop detected *)
-    ()
-  else
-    let new_visited = Hashtbl.copy visited in
-    Hashtbl.replace new_visited (i, j) (List.length pending_treasures);
-
-    if can_move_left grid i j then
-      traverse grid sources pending_treasures i (j - 1) (path_count + 1) new_visited;
-
-    if can_move_right grid i j then
-      traverse grid sources pending_treasures i (j + 1) (path_count + 1) new_visited;
-
-    if can_move_up grid i j then
-      traverse grid sources pending_treasures (i - 1) j (path_count + 1) new_visited;
-
-    if can_move_down grid i j then
-      traverse grid sources pending_treasures (i + 1) j (path_count + 1) new_visited
+(* Helper function to remove an element from a list *)
+let rec remove elem = function
+  | [] -> []
+  | hd :: tl -> if hd = elem then tl else hd :: remove elem tl
 
 let shortest_treasure_hunt grid =
   let sources = ref [] in
   let treasures = ref [] in
 
+  (* Collect sources and treasures *)
   for i = 0 to Array.length grid - 1 do
     for j = 0 to Array.length grid.(0) - 1 do
-      if grid.(i).(j) = 'S' then
-        sources := (i, j) :: !sources
-      else if grid.(i).(j) = 'T' then
-        treasures := (i, j) :: !treasures
+      match grid.(i).(j) with
+      | 'S' -> sources := (i, j) :: !sources
+      | 'T' -> treasures := (i, j) :: !treasures
+      | _ -> ()
     done
   done;
 
-  let visited = Hashtbl.create (Array.length grid * (Array.length grid.(0))) in
-  match !sources with
-  | (src_i, src_j) :: _ ->
-      traverse grid !sources !treasures src_i src_j 0 visited;
-      if List.length !possible_paths > 0 then
-        List.fold_left min max_int !possible_paths
-      else
-        -1
-  | [] -> -1
+  let directions = [(-1, 0); (1, 0); (0, -1); (0, 1)] in
+  let to_explore = Queue.create () in
+
+  (* Initialize the queue with all sources *)
+  List.iter (fun source ->
+    Queue.add (0, !treasures, source) to_explore) !sources;
+
+  let visited = Hashtbl.create 100 in
+
+  let rec bfs () =
+    (* If Queue is empty => No more paths *)
+    if Queue.is_empty to_explore then -1
+    else
+      let path_count, pending_treasures, (x, y) = Queue.pop to_explore in
+      (* If already visited try next *)
+      if Hashtbl.mem visited (x, y, pending_treasures) then bfs ()
+      else begin
+        (* Else explore neighbourhood *)
+        Hashtbl.add visited (x, y, pending_treasures) true;
+
+        let new_pending_treasures =
+          if grid.(x).(y) = 'T' then
+            remove (x, y) pending_treasures
+          else
+            pending_treasures
+        in
+
+        if grid.(x).(y) = 'S' && new_pending_treasures = [] then
+          path_count
+        else begin
+          List.iter (fun (dx, dy) ->
+            let nx, ny = x + dx, y + dy in
+            if can_move grid nx ny then
+              Queue.add (path_count + 1, new_pending_treasures, (nx, ny)) to_explore
+          ) directions;
+          bfs ()
+        end
+      end
+  in
+  bfs ()
+
 
 let () =
   assert ( shortest_treasure_hunt [|
@@ -103,10 +103,47 @@ let () =
     [| 'X'; 'X'; 'X' |];
     [| 'E'; 'E'; 'T' |]
   |] = -1 );
+  (* Has single source *)
   assert (
     shortest_treasure_hunt [|
     [| 'S'; 'E'; 'T' |];
     [| 'X'; 'T'; 'E' |];
     [| 'E'; 'E'; 'T' |]
   |] = 8 
+  );
+  (* Has loop + a treasure from seconds source *)
+  assert (
+    shortest_treasure_hunt [|
+      [|'S'; 'E'; 'X'; 'X'; 'X'; 'X'|];
+      [|'X'; 'E'; 'E'; 'E'; 'E'; 'X'|];
+      [|'X'; 'E'; 'X'; 'X'; 'E'; 'X'|];
+      [|'X'; 'E'; 'X'; 'X'; 'E'; 'X'|];
+      [|'X'; 'E'; 'E'; 'E'; 'E'; 'X'|];
+      [|'X'; 'X'; 'X'; 'X'; 'X'; 'X'|];
+      [|'E'; 'X'; 'X'; 'T'; 'S'; 'E'|]
+  |] = 2 
+  ); 
+  (* Has multiple source *) 
+  assert (
+    shortest_treasure_hunt [|
+      [|'X'; 'S'; 'X'|];
+      [|'X'; 'E'; 'X'|];
+      [|'X'; 'E'; 'S'|];
+      [|'S'; 'E'; 'X'|];
+      [|'X'; 'E'; 'X'|];
+      [|'X'; 'X'; 'X'|];
+      [|'E'; 'S'; 'T'|]
+  |] = 2 
+  );
+  (* Treasure is inaccesible *) 
+  assert (
+    shortest_treasure_hunt [|
+      [|'X'; 'S'; 'X'|];
+      [|'X'; 'E'; 'X'|];
+      [|'X'; 'E'; 'X'|];
+      [|'X'; 'E'; 'X'|];
+      [|'X'; 'E'; 'X'|];
+      [|'X'; 'X'; 'X'|];
+      [|'E'; 'E'; 'T'|]
+  |] = -1 
   );
